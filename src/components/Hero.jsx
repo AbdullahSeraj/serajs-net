a9554b0862a79bc874853a62a67141de2e7fed04
AbdullahSeraj <106870849+AbdullahSeraj@users.noreply.github.com>
2025-06-28 11:10:38 +0300

    Changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,14 +20,17 @@ const Hero = () => {
           <img src={logo} alt="" className='rounded-full w-[130px] border-2 border-yellow-500' />
           <MdVerified size={'25px'} className='text-blue-500' />
         </div>
-        <h1 className='text-4xl font-black mb-3'>Sotfware Designer, Computer Engineer, Software Developer, and Front-End Developer.</h1>
-        <p className='mb-5 text-gray-500'>Highly motivated and skilled Software Designer with a strong foundation in computer engineering and a passion for front-end development utilizing React.js and Vue.js. Currently pursuing a Computer Engineering degree at Al-Biruni University, demonstrating a commitment to continuous learning and growth in the ever-evolving field of software development. Eager to contribute to innovative projects that address complex problems and make a real-world impact.
+        <h1 className='text-4xl font-black mb-3'>💻 Expert Full Stack Developer | MERN Stack | Frontend Backend Developer</h1>
+        <p className='mb-5 text-gray-500 text-justify'>
+          I am a dedicated Full Stack Developer with a deep passion for crafting dynamic, responsive, and user-focused web applications. I thrive on turning complex problems into elegant solutions, blending creativity with technical expertise to deliver seamless and intuitive user experiences.
+
+          My development journey revolves around building scalable and maintainable applications using modern technologies across the full stack. Whether it is designing clean and interactive user interfaces or building robust backend systems, I enjoy taking ownership of the entire development process.
         </p>
         <div className='flex gap-5'>
           <a href="https://github.com/AbdullahSeraj" target='_blank'>
             <FaGithub size={'20px'} className='text-gray-500 cursor-pointer hover:text-black dark:hover:text-white transition-all' />
           </a>
-          <a href="https://linkedin.com/in/abdullah-seraj" target='_blank'>
+          <a href="https://www.linkedin.com/in/abdullah-seraj" target='_blank'>
             <BsLinkedin size={'20px'} className='text-gray-500 cursor-pointer hover:text-black dark:hover:text-white transition-all' />
           </a>
           <a href="https://wa.me/905013362511" target='_blank'>
